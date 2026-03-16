7c01d7d340ebf543ae5ed7ca0bbf3f8a2e663313
Moriam Akter Swarna <moriam15-12880@diu.edu.bd>
2026-03-16 13:25:45 +0600

    Video-8 font optimization included

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,26 @@
 import Link from "next/link";
 
+
+import { Inter, Poppins } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500"],
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["600", "700"],
+});
+
+export { inter, poppins };
+
+
 const AboutPage = () => {
  return (
  <div>
- <h1>This is About page </h1> 
+ <h1 className={inter.className}>This is About page </h1> 
+ <p className={poppins.className}> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit natus pariatur voluptates, atque officia soluta! Quod culpa pariatur corrupti magni, animi sint distinctio repellendus illum laboriosam vitae minus. Dignissimos, harum!</p>
 
  <Link href="/about/team">
  <button className="btn">Go to teamPage</button>
