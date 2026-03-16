88ca7eaa3cf4198f844046de5702801f894f1ed9
Moriam Akter Swarna <moriam15-12880@diu.edu.bd>
2026-03-16 13:46:57 +0600

    Video 9 catch all segment added

---
+++ src/app/docs/[...slug]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SlugPage = () => {
+    return (
+        <div>
+            <h1>Slug Page</h1>
+        </div>
+    );
+};
+
+export default SlugPage;
---
+++ src/app/documents/[[...docId]]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DocumentId = () => {
+    return (
+        <div>
+            <h1>Document ID Page</h1>
+        </div>
+    );
+};
+
+export default DocumentId;
